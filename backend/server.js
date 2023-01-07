fe1bf6034be457279238fc8676772c41208c1562
payalpagaria <pagariapayal67@gmail.com>
2023-01-07 21:47:28 +0530

    updated file

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 const express=require('express');
 const dotenv=require('dotenv').config();
 const app=express();
+
 const port=process.env.PORT||5000;
-app.get("/api/goals",(req,res)=>{
-    res.json({message:'get goals'});
-})
+app.use('/api/goals',require('./routes/getRoutes'))
 app.listen(port,()=>{
     console.log(`The server started on port ${port}`)
 })
