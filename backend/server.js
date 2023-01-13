09b66be263f9c9d22740b7b9204a51a5336172c2
payalpagaria <pagariapayal67@gmail.com>
2023-01-13 22:45:59 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const dotenv=require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+
 const app=express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -7,6 +9,7 @@ app.use(express.urlencoded({ extended: false }));
 const port=process.env.PORT||5000;
 
 app.use('/api/goals',require('./routes/getRoutes'))
+app.use(errorHandler);
 app.listen(port,()=>{
     console.log(`The server started on port ${port}`)
 })
